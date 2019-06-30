7f64057e869ca7c6c6d011a248e06d0e9c02120e
raminrez <ramin12207@gmail.com>
2019-07-01 01:02:18 +0430

    new constants structure

--- src/App.js
+++ src/App.js
@@ -6,14 +6,15 @@ import Search from "./components/Search";
 import Table from "./components/Table";
 import Button from "./components/Button";
 
-const DEFAULT_QUERY = "redux";
-const DEFAULT_HPP = "5";
-
-const PATH_BASE = "https://hn.algolia.com/api/v1";
-const PATH_SEARCH = "/search";
-const PARAM_SEARCH = "query=";
-const PARAM_PAGE = "page=";
-const PARAM_HPP = "hitsPerPage=";
+import {
+  DEFAULT_QUERY,
+  DEFAULT_HPP,
+  PATH_BASE,
+  PATH_SEARCH,
+  PARAM_SEARCH,
+  PARAM_PAGE,
+  PARAM_HPP
+} from "./constants/index.js";
 
 class App extends Component {
   _isMount = false;
