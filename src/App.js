424039da4fccb16f8a2ddbf6580cd97a870f3057
raminrez <ramin12207@gmail.com>
2019-07-02 00:07:39 +0430

    Loading SET

--- src/App.js
+++ src/App.js
@@ -24,7 +24,8 @@ class App extends Component {
       results: null,
       searchKey: "",
       searchTerm: DEFAULT_QUERY,
-      error: null
+      error: null,
+      isLoading: false
     };
 
     this.onDismiss = this.onDismiss.bind(this);
@@ -53,6 +54,7 @@ class App extends Component {
   }
 
   fetchSearchTopStories(searchTerm, page = 0) {
+    this.setState({ isLoading: true });
     axios(
       `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`
     )
@@ -71,7 +73,8 @@ class App extends Component {
       results: {
         ...results,
         [searchKey]: { hits: updatedHits, page }
-      }
+      },
+      isLoading: false
     });
   }
 
@@ -107,7 +110,7 @@ class App extends Component {
   };
 
   render() {
-    const { searchTerm, results, searchKey, error } = this.state;
+    const { searchTerm, results, searchKey, error, isLoading } = this.state;
     const page =
       (results && results[searchKey] && results[searchKey].page) || 0; //check current page value
 
@@ -131,15 +134,23 @@ class App extends Component {
           list && <Table onDismiss={this.onDismiss} list={list} />
         )}
         <div className="interactions">
-          <Button
-            onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}
-          >
-            More
-          </Button>
+          {isLoading ? (
+            <Loading />
+          ) : (
+            <Button
+              onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}
+            >
+              More
+            </Button>
+          )}
         </div>
       </div>
     );
   }
 }
+
+const Loading = () => {
+  return <div>Loading ...</div>;
+};
 
 export default App;
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-export default function Search({ value, onChange, children, onSubmit }) {
-  return (
-    <form onSubmit={onSubmit}>
-      <input type="text" value={value} onChange={onChange} />
-      <button type="submit">{children}</button>
-    </form>
-  );
+export default class Search extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    if (this.input) {
+      this.input.focus();
+    }
+  }
+
+  render() {
+    const { value, onChange, children, onSubmit } = this.props;
+    return (
+      <form onSubmit={onSubmit}>
+        <input
+          ref={node => (this.input = node)}
+          type="text"
+          value={value}
+          onChange={onChange}
+        />
+        <button type="submit">{children}</button>
+      </form>
+    );
+  }
 }
 
 Search.propTypes = {
